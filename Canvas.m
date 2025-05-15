005f2e55a15212b42195bf7798be44e91e0992a8
Devon <lantagned@msoe.edu>
2025-05-15 15:36:46 -0500

    Added create/delete tests

--- Canvas.m
+++ Canvas.m
@@ -658,16 +658,135 @@ classdef Canvas
             [module, status, resp] = deleteObject(obj, url);
 
         end
-        function [module, status, resp] = createModuleItem(obj, moduleID, itemType)
+        function [moduleItem, status, resp] = createModuleItem(obj, moduleID, itemType, opts)
             arguments
                 obj (1,1) Canvas
                 moduleID (1,1) string
                 itemType (1,1) string {mustBeMember(itemType,...
                     ["File", "Page", "Discussion", "Assignment", "Quiz", ...
                     "SubHeader", "ExternalUrl", "ExternalTool"])}
+                opts.Title string = ""
+                opts.ContentID string = ""
+                opts.Position = []
+                opts.Indent = []
+                opts.PageURL string = ""
+                opts.ExternalURL string = ""
+                opts.Publish (1,1) logical = false
             end
 
-            
+            % First check requirements depending on selected itemType
+
+            % Required for ContentID
+            if ismember(itemType, ["File", "Discussion", "Assignment", "Quiz", "ExternalTool"])
+                if isempty(opts.ContentID)
+                    error("ContentID must be defined for itemType=%s", itemType)
+                end
+            end
+
+            % For Page
+            if itemType == "Page"
+                if opts.PageURL == ""
+                    error("PageURL must be defined for itemType=%s", itemType)
+                end
+            end
+
+            % For ExternalURL
+            if ismember(itemType, ["ExternalUrl", "ExternalTool"])
+                if opts.ExternalURL == ""
+                    error("ExternalURL must be defined for itemType=%s", itemType)
+                end
+            end
+
+            % Now build the request
+
+            endpoint = "modules/" + moduleID + "/items";
+            url = buildURL(obj, endpoint);
+
+            formArgs = {"module_item[type]", itemType};
+
+            if opts.Title ~= ""
+                formArgs = [formArgs, {"module_item[title]", opts.Title}];
+            end
+
+            if opts.ContentID ~= ""
+                formArgs = [formArgs, {"module_item[content_id]", opts.ContentID}];
+            end
+
+            if ~isempty(opts.Position)
+                formArgs = [formArgs, {"module_item[position]", string(opts.Position)}];
+            end
+
+            if ~isempty(opts.Indent)
+                formArgs = [formArgs, {"module_item[indent]", string(opts.Indent)}];
+            end
+
+            if opts.PageURL ~= ""
+                formArgs = [formArgs, {"module_item[page_url]", opts.PageURL}];
+            end
+
+            if opts.ExternalURL ~= ""
+                formArgs = [formArgs, {"module_item[external_url]", opts.ExternalURL}];
+            end
+
+            form = matlab.net.http.io.FormProvider(formArgs{:});
+
+            [moduleItem, status, resp] = postPayload(obj, url, form);
+
+            if opts.Publish && ~isempty(moduleItem)
+                % send update request right away
+                [moduleItem, status, resp] = updateModuleItem(obj, ...
+                    moduleID, moduleItem.id, Published=true);
+            end
+
+        end
+        function [moduleItem, status, resp] = updateModuleItem(obj, moduleID, itemID, opts)
+            arguments
+                obj (1,1) Canvas
+                moduleID (1,1) string
+                itemID (1,1) string
+                opts.Title string = ""
+                opts.Position = []
+                opts.Indent = []
+                opts.ExternalURL string = ""
+                opts.Published logical = []
+                opts.NewModuleID string = ""
+            end
+
+            % Now build the request
+
+            endpoint = "modules/" + moduleID + "/items/" + itemID;
+            url = buildURL(obj, endpoint);
+
+            formArgs = {};
+
+            if opts.Title ~= ""
+                formArgs = [formArgs, {"module_item[title]", opts.Title}];
+            end
+
+            if ~isempty(opts.Position)
+                formArgs = [formArgs, {"module_item[position]", string(opts.Position)}];
+            end
+
+            if ~isempty(opts.Indent)
+                formArgs = [formArgs, {"module_item[indent]", string(opts.Indent)}];
+            end
+
+            if opts.ExternalURL ~= ""
+                formArgs = [formArgs, {"module_item[external_url]", opts.ExternalURL}];
+            end
+
+            if ~isempty(opts.Published)
+                formArgs = [formArgs, {"module_item[published]", string(opts.Published)}];
+            end
+
+            if opts.NewModuleID ~= ""
+                formArgs = [formArgs, {"module_item[module_id]", opts.NewModuleID}];
+            end
+
+            form = matlab.net.http.io.FormProvider(formArgs{:});
+
+            [moduleItem, status, resp] = putPayload(obj, url, form);
+
         end
     
 
--- testURLsPUT.m
+++ testURLsPUT.m
@@ -10,13 +10,11 @@ canv = Canvas(env.BASE_URL, env.API_KEY, env.COURSE_ID, debug=true)
 fprintf("Canvas API connected!\n")
 
 %% File Upload Test
-% Uploads the testFile.txt to Canvas, waits for user input (to check
-% Canvas), and then deletes the file
-testFileName = "testFile.txt";
+% Uploads the testFile.txt to Canvas.
 
-fprintf("--- Starting Upload/Delete File Test ---\n")
+fprintf("--- Starting Upload File Test ---\n")
 
-fileInfo = dir(testFileName);
+fileInfo = dir("testFile.txt");
 if isempty(fileInfo); error("File not found."); end
 testFile = fullfile(fileInfo.folder, fileInfo.name);
 
@@ -25,22 +23,35 @@ fprintf("Uploading File...\n")
 file = canv.uploadFile(endpoint, testFile);
 fprintf("File uploaded. Check Canvas.\n")
 
-input("Press ENTER to continue");
+%% File Delete Test
+% Deletes the testFile.txt that was uploaded with the upload test.
 
-fprintf("Deleting File...\n")
-[~,status] = canv.deleteFile(file.id);
-fprintf("File deleted. Check Canvas.\n")
+fprintf("--- Starting Delete File Test ---\n")
 
+fprintf("Searching for testFile.txt on Canvas...\n")
+file = canv.getFiles(Search="testFile.txt");
+if isempty(file)
+    fprintf("File not found.\n")
+else
+    fprintf("File found.\nDeleting File...\n")
+    [~,status] = canv.deleteFile(file.id);
+    fprintf("File deleted.\n")
+end
 
 %% Test Create Module
-% This test creates a module, waits for user input (to check Canvas), and
-% then deletes the module. The unlock time will be 1 hour from time of
-% execution.
+% This test creates a module and items, waits for user input (to check
+% Canvas), and then deletes the module. The unlock time will be 1 hour from
+% time of execution.
 fprintf("--- Starting Creation/Delete Module Test ---\n")
 
 fprintf("Creating module...\n")
 NewModule = canv.createModule("Test Module 01", "UnlockAt", datetime()+hours(1));
 fprintf("Module created. Check Canvas.\n")
+
+fprintf("Creating module item (subheader)...\n")
+NewModuleItem = canv.createModuleItem(NewModule.id, "SubHeader", ...
+    Title="Test Subheader", Publish=true);
+fprintf("Module item created. Check Canvas.\n")
 
 input("Press ENTER to continue");
 
