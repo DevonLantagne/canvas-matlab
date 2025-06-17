54385169a407e297e3269f40e547b71fb463c81f
Devon <devonlantagne@me.com>
2025-06-17 14:35:18 -0500

    Modified arguments for getStudent and smart search

--- Canvas.m
+++ Canvas.m
@@ -121,49 +121,85 @@ classdef Canvas
     methods
         % Course
         function [course, status, resp] = getCourse(obj)
-            
+            url = buildURL(obj);
+            [course, status, resp] = getPayload(obj, url);
+            course = Chars2StringsRec(course);
         end
-        
+        function [object, status, resp] = smartSearch(obj, search, filter)
+            arguments
+                obj (1,1) Canvas
+                search (1,1) string
+                filter (1,:) string {mustBeMember(filter, ["pages", "assignments", "announcements", "discussion_topics"])}
+            end
+
+            opts.search = search;
+            opts.filter = filter;
+
+            endpoint = "smartsearch";
+
+            argSet = buildArgSet( ...
+                "search",   "q",        "string", ...
+                "filter",   "filter[]", "string");
+
+            queries = buildBody("get", opts, argSet);
+
+            url = buildURL(obj, endpoint);
+
+            [object, status, resp] = getPayload(obj, url, queries);
+        end
+
         % Students
-        function [students, status, resp] = getStudents(obj, opts)
+        function [students, status, resp] = getStudents(obj, qopts, opts)
             %GETSTUDENTS Retrieve all active students enrolled in the course
             %   students = getStudents(obj) fetches active enrolled students in the current course.
             %
             %   Optional name-value pairs:
             %       GetAvatar - if true, includes avatar URLs in the response
-            %       Query - if empty, does not send default query params.
-            %               User can supply a cell array of name,values:
-            %               st = canv.getStudents(Query={'arg1','val1','arg2','val2'})
-
-            % TODO: revamp optional arguments to be consistent
 
             arguments
                 obj (1,1) Canvas
+
+                qopts.Search string = []
+                qopts.Sort (1,1) string {mustBeMember(qopts.Sort, ...
+                    ["username", "last_login", "email", "sis_id"])} = "username"
+                qopts.EnrollmentType string {mustBeMember(qopts.EnrollmentType, ...
+                    ["teacher", "student", "student_view", "ta", "observer", "designer"])} = "student"
+                qopts.UserIDs (1,:) string = []
+                qopts.EnrollmentState (1,:) string {mustBeMember(qopts.EnrollmentState, ...
+                    ["active", "invited", "rejected", "completed", "inactive"])} = "active"
+                
                 opts.GetAvatar (1,1) logical = false
-                opts.Query = 0;
+                opts.GetBio (1,1) logical = false
+                opts.GetTestStudent (1,1) logical = false
             end
 
             endpoint = "search_users";
+            url = obj.buildURL(endpoint);
 
-            % Determine query args
-            if opts.Query == 0
-                % Use defaults
-                qry = {'enrollment_type[]',   'student',...
-                    'enrollment_state[]',  'active',...
-                    'per_page',            obj.perPage,...
-                    'include[]',           'enrollments'};
-                % Append avatar links if needed
-                if opts.GetAvatar
-                    qry = [qry, {'include[]', 'avatar_url'}];
-                end
-            else
-                % Use User's (can be empty for no queries)
-                qry = opts.Query;
+            ExtraQueryArgs = {...
+                'per_page',     obj.perPage, ...
+                'include[]',    'enrollments'};
+
+            if opts.GetAvatar
+                ExtraQueryArgs = [ExtraQueryArgs, {'include[]', 'avatar_url'}];
+            end
+            if opts.GetBio
+                ExtraQueryArgs = [ExtraQueryArgs, {'include[]', 'bio'}];
+            end
+            if opts.GetTestStudent
+                ExtraQueryArgs = [ExtraQueryArgs, {'include[]', 'test_student'}];
             end
 
-            url = obj.buildURL(endpoint, qry);
+            argSet = buildArgSet( ...
+                "Search",           "search_term",          "string", ...
+                "Sort",             "sort",                 "string", ...
+                "EnrollmentType",   "enrollment_type[]",    "string", ...
+                "UserIDs",          "user_ids[]",           "integer", ...
+                "EnrollmentState",  "enrollment_state[]",   "string");
 
-            [students, status, resp] = getPayload(obj, url);
+            queries = buildBody("get", qopts, argSet, LastArgs=ExtraQueryArgs);
+
+            [students, status, resp] = getPayload(obj, url, queries);
 
             % Modify data output by relocating enrollment section (if it is
             % in the response)
@@ -175,7 +211,6 @@ classdef Canvas
             end
 
             students = Chars2StringsRec(students);
-
         end
 
         % Assignments and Weighting
